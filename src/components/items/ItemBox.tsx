89bbd6f31ae066f492e6a23ecf97ac5e1a288f01
Gyeongbin Bak <tarabin49@gmail.com>
2023-11-27 03:10:52 +0900

    [FEAT]Item Grid 정렬 issue #1

--- src/components/items/ItemBox.tsx
+++ src/components/items/ItemBox.tsx
@@ -4,13 +4,100 @@ import styled from 'styled-components'
 const ItemBox = () => {
   return (
     <>
-    <GetColor>ItemBox</GetColor>
+    <GridScroll>
+        <GridBox>
+            <ItemGridBox>T</ItemGridBox>
+            <ItemGridBox>T</ItemGridBox>
+            <ItemGridBox>T</ItemGridBox>
+            <ItemGridBox>T</ItemGridBox>
+            <ItemGridBox>T</ItemGridBox>
+            <ItemGridBox>T</ItemGridBox>
+            <ItemGridBox>T</ItemGridBox>
+            <ItemGridBox>T</ItemGridBox>
+            <ItemGridBox>T</ItemGridBox>
+            <ItemGridBox>T</ItemGridBox>
+            <ItemGridBox>T</ItemGridBox>
+            <ItemGridBox>T</ItemGridBox>
+            <ItemGridBox>T</ItemGridBox>
+            <ItemGridBox>T</ItemGridBox>
+            <ItemGridBox>T</ItemGridBox>
+            <ItemGridBox>T</ItemGridBox>
+            <ItemGridBox>T</ItemGridBox>
+            <ItemGridBox>T</ItemGridBox>
+            <ItemGridBox>T</ItemGridBox>
+            <ItemGridBox>T</ItemGridBox>
+            <ItemGridBox>T</ItemGridBox>
+            <ItemGridBox>T</ItemGridBox>
+            <ItemGridBox>T</ItemGridBox>
+            <ItemGridBox>T</ItemGridBox>
+            <ItemGridBox>T</ItemGridBox>
+            <ItemGridBox>T</ItemGridBox>
+            <ItemGridBox>T</ItemGridBox>
+            <ItemGridBox>T</ItemGridBox>
+            <ItemGridBox>T</ItemGridBox>
+            <ItemGridBox>T</ItemGridBox>
+            <ItemGridBox>T</ItemGridBox>
+            <ItemGridBox>T</ItemGridBox>
+            <ItemGridBox>T</ItemGridBox>
+            <ItemGridBox>T</ItemGridBox>
+            <ItemGridBox>T</ItemGridBox>
+            <ItemGridBox>T</ItemGridBox>
+            <ItemGridBox>T</ItemGridBox>
+            <ItemGridBox>T</ItemGridBox>
+            <ItemGridBox>T</ItemGridBox>
+            <ItemGridBox>T</ItemGridBox>
+            <ItemGridBox>T</ItemGridBox>
+            <ItemGridBox>T</ItemGridBox>
+            <ItemGridBox>T</ItemGridBox>
+            <ItemGridBox>T</ItemGridBox>
+            <ItemGridBox>T</ItemGridBox>
+            <ItemGridBox>T</ItemGridBox>
+            <ItemGridBox>T</ItemGridBox>
+            <ItemGridBox>T</ItemGridBox>
+            <ItemGridBox>T</ItemGridBox>
+            <ItemGridBox>T</ItemGridBox>
+            <ItemGridBox>T</ItemGridBox>
+        </GridBox>
+    </GridScroll>
     </>
   )
 }
 
 export default ItemBox
 
-const GetColor = styled.div`
-    color: white;
+const GridScroll = styled.div`
+    display: flex;
+    overflow-y: scroll;
+    justify-content: center;
+    /* align-items: center; */
+    width: fit-content;
+    height: 300px;
+    background-color: black;
+    border: 10px solid red;
+`
+
+const GridBox = styled.div`
+    display: grid;
+    height: fit-content;
+    /* border: 1px solid red; */
+    background-color: black;
+    //450px기준으로
+    grid-template-columns: repeat(7, 70px);
+    /* grid-template-rows: repeat(3,1fr); */
+    grid-auto-flow: row;
+    grid-gap: 2px;
+    padding: 2px;
+    margin-right: 10px;
+    /* overflow-y: scroll;
+    &::-webkit-scrollbar {
+        display: none;
+    } */
+`
+
+const ItemGridBox = styled.div`
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    height: 63px;
+    background-color: gray;
 `
