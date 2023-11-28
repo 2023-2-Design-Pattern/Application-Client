0c6a322b9234d1d63ee58f14391889d0fe9e4111
hnnynh <hnnynh125@gmail.com>
2023-11-28 20:12:04 +0900

    [FEAT] 조작 설명 추가, 캐릭터 좌우 이동 표시

--- src/components/pages/Landing.tsx
+++ src/components/pages/Landing.tsx
@@ -1,113 +1,222 @@
-import React, { useEffect, useState } from 'react'
-import styled from 'styled-components'
-import { useNavigate } from 'react-router-dom';
+import React, { useEffect, useState } from "react";
+import styled from "styled-components";
+import { useNavigate } from "react-router-dom";
 
 const Landing = () => {
-    const [userName, setUserName] = useState('');
-    const [btnActive, setBtnActive] = useState('');
-    // const setUserNameRecoil = useSetRecoilState(userNameAtom);
+  const [userName, setUserName] = useState("");
+  const [btnActive, setBtnActive] = useState("");
+  // const setUserNameRecoil = useSetRecoilState(userNameAtom);
 
-    const navigate = useNavigate();
+  const navigate = useNavigate();
 
-    useEffect(()=>{
-        if(userName === ''){
-            setBtnActive('');
-        } else {
-            setBtnActive('active');
-        }
-    }, [userName])
-
-    const onChangeInput = (e:React.ChangeEvent<HTMLInputElement>) => {
-        setUserName(e.target.value);
+  useEffect(() => {
+    if (userName === "") {
+      setBtnActive("");
+    } else {
+      setBtnActive("active");
     }
-    const onClickEvent = async() => {
-        if(userName !== ''){
-            console.log(userName);
-            //API 호출
-            // const response = await postUser(userName);
-            // if(response === false){
-            //     console.log('error');
-            // } else {
-            //     setUserNameRecoil(userName);
-            // }
-            navigate(`/game`);
-        }
-    };
+  }, [userName]);
+
+  const onChangeInput = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setUserName(e.target.value);
+  };
+  const onClickEvent = async () => {
+    if (userName !== "") {
+      console.log(userName);
+      //API 호출
+      // const response = await postUser(userName);
+      // if(response === false){
+      //     console.log('error');
+      // } else {
+      //     setUserNameRecoil(userName);
+      // }
+      navigate(`/game`);
+    }
+  };
   return (
     <Wrapper>
-        <div className='title'>Christmas Mare</div>
-        <GetUserNameWrapper>
-            <div className='username-label'>UserName</div>
-            <UserNameInputBox>
-                <input placeholder='Enter Your NickName' value={userName} onChange={(e)=>onChangeInput(e)} />
-                <Button className={btnActive} onClick={() => onClickEvent()}>Go!</Button>
-            </UserNameInputBox>
-        </GetUserNameWrapper>
-    </Wrapper>
-  )
-}
+      <LeftWrapper>
+        <div className="action">🢀 🢂 🢁 🢃</div>
+        <div className="des">Move</div>
+        <div className="action">Drag & Drop</div>
+        <div className="des">Upgrade or Use Items</div>
+        <div className="action">First Level Items</div>
+        <ItemWrapper>
+          <img src="./public/img/item-3.png" className="item"></img>
+          <img src="./public/img/item-4.png" className="item"></img>
+          <img src="./public/img/item-5.png" className="item"></img>
+        </ItemWrapper>
+        <div className="action">Second Level Items</div>
+        <ItemWrapper>
+          <img src="./public/img/item-6.png" className="item"></img>
+          <img src="./public/img/item-7.png" className="item"></img>
+          <img src="./public/img/item-8.png" className="item"></img>
+        </ItemWrapper>
+        <div className="action">What will it be?</div>
+        <ItemWrapper>
+          <img src="./public/img/item-3.png" className="item"></img>
+          <div className="item-for">+</div>
+          <img src="./public/img/item-3.png" className="item"></img>
+          <div className="item-for">=</div>
+          <div className="item-for">?</div>
+        </ItemWrapper>
+      </LeftWrapper>
 
-export default Landing
+      <RightWrapper>
+        <img src="./public/img/ghost.png" className="character"></img>
+        <TitleWrapper>
+          <img src="./public/img/item-4.png" className="title-img"></img>
+          <div className="title">Christmas Mare</div>
+          <img src="./public/img/item-4.png" className="title-img"></img>
+        </TitleWrapper>
+        <GetUserNameWrapper>
+          <div className="username-label">UserName : </div>
+          <UserNameInputBox>
+            <input
+              placeholder="Enter Your NickName"
+              value={userName}
+              onChange={(e) => onChangeInput(e)}
+            />
+            <Button className={btnActive} onClick={() => onClickEvent()}>
+              Go!
+            </Button>
+          </UserNameInputBox>
+        </GetUserNameWrapper>
+      </RightWrapper>
+    </Wrapper>
+  );
+};
+
+export default Landing;
 
 const Wrapper = styled.div`
-    display: flex;
-    width: 100%;
-    flex-direction: column;
-    justify-content: center;
-    align-items: center;
-    padding-top: 280px;
-    gap: 50px;
-    & > .title{
-        font-size: 100px;
-        color: white;
-    }
-`
+  display: flex;
+  width: 100%;
+  flex-direction: row;
+`;
 
 const GetUserNameWrapper = styled.div`
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    width: 60%;
-    gap: 10px;
-    & > .username-label{
-        font-size: 30px;
-        color: white;
-    }
-`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 70%;
+  gap: 10px;
+  & > .username-label {
+    font-size: 30px;
+    color: white;
+  }
+`;
 
 const UserNameInputBox = styled.div`
-    display: flex;
-    width: 70%;
-    background-color: white;
-    border-radius: 50px;
+  display: flex;
+  width: 50%;
+  background-color: white;
+  border-radius: 20px;
+  padding: 10px 10px 10px 5px;
+  & > input {
+    width: 80%;
+    border-radius: 20px;
+    border: none;
+    margin-left: 20px;
     padding: 10px 10px 10px 5px;
-    & > input{
-        width: 80%;
-        border-radius: 50px;
-        border: none;
-        margin-left: 20px;
-        padding: 10px 10px 10px 5px;
-        font-size: 20px;
-        &:focus{
-            outline: none;
-        }
+    font-size: 20px;
+    &:focus {
+      outline: none;
     }
-`
+  }
+`;
 
 const Button = styled.div`
+  display: flex;
+  width: 20%;
+  background-color: #c9c9c9;
+  border-radius: 10px;
+  font-size: 20px;
+  justify-content: center;
+  align-items: center;
+  color: #969696;
+  &.active {
+    background-color: red;
+    color: white;
+    &:hover {
+      cursor: pointer;
+    }
+  }
+`;
+
+const TitleWrapper = styled.div`
+  display: flex;
+  width: 80%;
+  flex-direction: row;
+  justify-content: center;
+  align-items: center;
+  & > .title {
+    font-size: 100px;
+    color: lightgrey;
+    padding-top: 15px;
+    padding-bottom: 30px;
+  }
+  & > .title-img {
+    width: 70px;
+    height: 70px;
+    padding: 15px 30px 20px 20px;
+    padding-top: 15px;
+    padding-bottom: 30px;
+  }
+`;
+
+const RightWrapper = styled.div`
+  display: flex;
+  width: 80%;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  margin-right: 5%;
+  & > .character {
     display: flex;
-    width: 20%;
-    background-color: #C9C9C9;
-    border-radius: 20px;
-    font-size: 20px;
+    padding-top: 90px;
+    width: 300px;
+    height: 300px;
     justify-content: center;
     align-items: center;
-    color: #969696;
-    &.active{
-        background-color: red;
-        color: white;
-        &:hover{
-            cursor: pointer;
-        }
-    }
-`
+  }
+`;
+
+const LeftWrapper = styled.div`
+  display: flex;
+  width: 20%;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  gap: 20px;
+  padding-left: 50px;
+  padding-top: 60px;
+  color: white;
+  & > .action {
+    font-size: 30px;
+  }
+  & > .des {
+    color: grey;
+    font-size: 20px;
+    padding-bottom: 20px;
+  }
+`;
+
+const ItemWrapper = styled.div`
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+  align-items: center;
+  gap: 20px;
+  padding-bottom: 20px;
+
+  & > .item {
+    font-size: 20px;
+    width: 30px;
+    height: 30px;
+  }
+  & > .item-for {
+    font-size: 20px;
+    color: white;
+  }
+`;
